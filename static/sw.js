42238c581738a23ab5289320d29106ced6006219
PierreCsn <cosson.pierre@gmail.com>
2026-09-01 21:58:22 +0200

    Fix Thermomix icon sizing: set explicit HTML dimensions and make list badge sleek and discreet

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cookgram-v13';
+const CACHE = 'cookgram-v14';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './app.css', './app.js']))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }))));
